769712d2d3454a77ecb5b7eace76a56497ba1fed
Jordi van der Lem <43111967+jvdlem@users.noreply.github.com>
2023-06-06 10:58:19 +0200

    chaged resources?

--- Choris Anchos/Assets/Scripts/ShaderInteractor.cs
+++ Choris Anchos/Assets/Scripts/ShaderInteractor.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEngine.GraphicsBuffer;
 
 [ExecuteInEditMode]
 public class ShaderInteractor : MonoBehaviour
 {
+    public GameObject player;
     public float playerSize;
-    public List<GrassObject> objectList; 
-    public GameObject objects;
-    public GameObject objects2;
+    public float playerInteractionRange = 10;
+    public List<GrassObject> objectList;
+    public float amountOfRenderedObjects = 8;
     public int index;
+    private float tempDistance;
 
 
 
@@ -18,12 +23,17 @@ public class ShaderInteractor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < objectList.Count; i++)
+
+        // Sort the objectsList based on the distances to the target position
+        objectList.Sort((a, b) => Vector3.Distance(a.gameObject.transform.position, player.transform.position).CompareTo(Vector3.Distance(b.gameObject.transform.position, player.transform.position)));
+        for (int i = 0; i < amountOfRenderedObjects; i++)
         {
+
             Shader.SetGlobalVector("_Interactor" + i, objectList[i].gameObject.transform.position + Vector3.up * objectList[i].playerSize);
             Vector3 position = objectList[i].gameObject.transform.position + Vector3.up * objectList[i].playerSize;
             Vector4 ShaderValues = new Vector4(position.x, position.y, position.z, objectList[i].InteractionRange);
             Shader.SetGlobalVector("_Tester" + i, ShaderValues);
+
         }
 
     }
