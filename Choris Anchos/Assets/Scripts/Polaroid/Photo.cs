d5787cb75c6e9950d3fe47922f50536551c71d24
Juacquesch <Juacquesch@hotmail.com>
2023-06-22 01:12:39 +0200

    Updated trees LOD, updated sockets. Added Polaroid and added photo. Everything works. Is correctly socketable and correct handposes. Only needing to add grass and needing to fix fps issues. Then done.

---
+++ Choris Anchos/Assets/Scripts/Polaroid/Photo.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// [RequireComponent(typeof(ApplyPhysics))]
+public class Photo : MonoBehaviour
+{
+    public MeshRenderer imageRenderer = null;
+    Color colorStart = Color.black;
+    Color colorEnd = Color.white;
+    public float fadeDuration = 1.0f;
+    public GameObject polaroid;
+    private bool isGrabbed = false;
+    private bool donePrinting = false;
+
+
+    private Collider currentCollider = null;
+    // private ApplyPhysics applyPhysics = null;
+
+    private void Awake()
+    {
+        currentCollider = GetComponent<Collider>();
+        // applyPhysics = GetComponent<ApplyPhysics>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(EjectOverSeconds(1.5f));        
+        StartCoroutine(FadeObject());
+    }
+
+    private void Update()
+    {
+        if (!isGrabbed && donePrinting)
+        {
+            this.transform.position = polaroid.GetComponent<Polaroid>().photoLocation.transform.position;
+            this.transform.rotation = polaroid.GetComponent<Polaroid>().photoLocation.transform.rotation;
+        }
+    }
+
+    public void IsGrabbed(bool grabbed)
+    {
+        this.isGrabbed = grabbed;
+    }
+
+    public IEnumerator EjectOverSeconds(float seconds)
+    {
+        // applyPhysics.DisablePhysics();
+        currentCollider.enabled = false;
+
+        float elapsedTime = 0;
+        while (elapsedTime < seconds)
+        {
+            transform.position += transform.forward * Time.deltaTime * 0.15f;
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        donePrinting = true;
+        currentCollider.enabled = true;
+    }
+
+    public IEnumerator FadeObject(){
+        float t = 0.0f;
+        while (t < fadeDuration){
+            t += Time.deltaTime;
+            imageRenderer.material.color = Color.Lerp(colorStart, colorEnd, t / fadeDuration);
+            yield return null;
+        }
+    }
+
+    public void SetImage(Texture2D texture)
+    {
+        // imageRenderer.material.color = Color.white;
+        imageRenderer.material.mainTexture = texture;
+    }
+}
---
+++ Choris Anchos/Assets/Scripts/Polaroid/Polaroid.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR;
+using HurricaneVR.Framework.ControllerInput;
+
+public class Polaroid : MonoBehaviour
+{
+    public HVRPlayerInputs playerInputs;
+
+    public GameObject photoPrefab = null;
+    public MeshRenderer screenRenderer = null;
+    public Transform spawnLocation = null;
+    public GameObject photos;
+    public GameObject photoLocation;
+    public int counter = 10;
+    
+    private bool filmCartridge = true;
+
+    private Camera renderCamera = null;
+    private bool isGrabbed = false;
+    private bool photoTaken = false;
+
+    public TMP_Text frameCount;
+
+    private void Awake()
+    {
+        renderCamera = GetComponentInChildren<Camera>();
+    }
+
+    private void Start()
+    {
+        CreateRenderTexture();
+    }
+
+    void Update() {
+        bool rightTrigger = playerInputs.RightController.TriggerButtonState.Active;
+        bool leftTrigger = playerInputs.LeftController.TriggerButtonState.Active;
+
+        frameCount.text = counter.ToString();
+
+        if (leftTrigger)
+            Debug.Log("Left trigger pressed");
+
+        if (rightTrigger)
+            Debug.Log("Right trigger pressed");
+
+        if (isGrabbed)
+            Debug.Log("Polaroid is grabbed");
+
+        if ((rightTrigger || leftTrigger) && isGrabbed && !photoTaken)
+        {
+            TakePhoto();
+            photoTaken = true;
+        }
+
+        if (!rightTrigger && !leftTrigger)
+            photoTaken = false;
+    }
+
+    public void IsGrabbed(bool grabbed)
+    {
+        this.isGrabbed = grabbed;
+    }
+
+    private void CreateRenderTexture()
+    {
+        RenderTexture newTexture = new RenderTexture(256, 256, 32, RenderTextureFormat.Default, RenderTextureReadWrite.sRGB);
+        newTexture.antiAliasing = 4;
+
+        renderCamera.targetTexture = newTexture;
+        screenRenderer.material.mainTexture = newTexture;
+    }
+
+    public void TakePhoto()
+    {
+        Debug.Log(filmCartridge);
+        photoCounter();
+        if(filmCartridge){
+            Photo newPhoto = CreatePhoto();
+            SetPhotoImage(newPhoto);
+        }
+    }
+
+    private Photo CreatePhoto()
+    {
+        GameObject photoObject = Instantiate(photoPrefab, spawnLocation.position, spawnLocation.rotation, transform);
+        photoObject.transform.parent = photos.transform;
+        photoObject.SetActive(true);
+        photoObject.GetComponent<Collider>().enabled = true;
+        return photoObject.GetComponent<Photo>();
+    }
+
+    private void SetPhotoImage(Photo photo)
+    {
+        Texture2D newTexture = RenderCameraToTexture(renderCamera);
+        photo.SetImage(newTexture);
+    }
+
+    private Texture2D RenderCameraToTexture(Camera camera)
+    {
+        camera.Render();
+        RenderTexture.active = camera.targetTexture;
+
+        Texture2D photo = new Texture2D(256, 256, TextureFormat.RGB24, false);
+        photo.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+        photo.Apply();
+
+        return photo;
+    }
+
+    public void photoCounter(){
+        counter--;
+        Debug.Log(counter);
+
+        if(counter < 0){
+            filmCartridge = false;
+            counter = 0;
+        }
+    }
+}
